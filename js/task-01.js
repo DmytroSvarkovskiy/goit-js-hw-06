b8690afb576b93cc9aaaac4adcaea3ca85aac27c
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-11 10:47:48 +0300

    Виконав 2 задачі

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const liItem = document.querySelectorAll('.item');
+const liItemQuantity=()=> {
+   console.log(`Number of categories: ${liItem.length}`);
+}
+liItemQuantity();
+
+
+const quantityCategories = (liItem)=> {
+    liItem.forEach(item => {
+        const title = item.firstElementChild.textContent;
+        const quantityElement = item.querySelector('ul').children.length;
+        console.log(`Category: ${title}
+Elements ${quantityElement}`);
+    })
+   
+}
+quantityCategories(liItem);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+
+const createEl = (ingredients) => {
+  const addEl = ingredients.map(item => {
+    const liEl = document.createElement("li");
+    liEl.classList.add('item');
+    liEl.textContent = item;
+    return liEl;
+  });
+  return addEl;
+}
+
+const newElements = (createEl(ingredients));
+ulEl.append(...newElements);
